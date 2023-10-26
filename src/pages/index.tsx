cacc29451c1efd0094454a51e4e0742a77e6dc5c
toasahi <asa1313to@gmail.com>
2023-10-27 07:09:04 +0900

    ♻️ handle**の引数の名前を変更

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,7 @@ export default function Home() {
 
   const handleClick = useCallback(() => {
     if(count < 10){
-      setCount(count => count+1)
+      setCount(prevCount => prevCount+1)
     } 
   },[count])
   const handleChange = (e:ChangeEvent<HTMLInputElement>) => {
@@ -22,7 +22,7 @@ export default function Home() {
   }
 
   const handleDisplay = useCallback(()=>{
-    setIsShow((isShow)=>!isShow)
+    setIsShow((prevIsShow)=>!prevIsShow)
   },[])
 
   useEffect(()=> {
