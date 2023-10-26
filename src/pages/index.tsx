724bd2fd647e4fabfa313adcb23ff55c2999c42e
toasahi <asa1313to@gmail.com>
2023-10-26 20:17:21 +0900

    ✨ useeffectの機能を追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,15 @@
 import { Main } from '@/src/components/Main'
 import Head from 'next/head'
+import { useEffect } from 'react'
 
 export default function Home() {
+  useEffect(()=> {
+    document.body.style.backgroundColor="lightblue"
+
+    return() => {
+      document.body.style.backgroundColor=""
+    }
+  },[])
   return (
     <>
       <Head>
