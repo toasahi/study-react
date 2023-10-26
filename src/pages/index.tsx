76e4e8f5aaca1e6cdefc733c2d06350cb36f0e91
toasahi <asa1313to@gmail.com>
2023-10-26 20:36:03 +0900

    ✨ Couterを追加してUsestateを追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,13 @@
 import { Main } from '@/src/components/Main'
 import Head from 'next/head'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 export default function Home() {
+  const [count,setCount] = useState(0)
+  const handleClick = () => {
+      setCount(count => count+1)
+      setCount(count => count+1)
+  }
   useEffect(()=> {
     document.body.style.backgroundColor="lightblue"
 
@@ -15,6 +20,8 @@ export default function Home() {
       <Head>
         <title>Index</title>
       </Head>
+      <h2>{count}</h2>
+      <button onClick={handleClick}>ボタン</button>
       <Main page='index'/>
     </>
   )
