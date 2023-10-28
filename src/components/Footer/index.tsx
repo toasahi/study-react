5227dc543f043d3be7d22379ed6e2bf22f4ca983
toasahi <asa1313to@gmail.com>
2023-10-28 18:17:54 +0900

    🎨 コンポーネント間のstateのリフトアップ

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,34 +1,22 @@
 
 import { Card } from '@/src/components/Card/Card'
 import classes from '@/src/components/Footer/Footer.module.css'
+import { useCallback, useState } from 'react'
 
-const ITEMS = [
-  {
-    href : "https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app",
-    title : "Docs",
-    description : "Find in-depth information about Next.js features and&nbsp;API."
-  },
-  {
-    href : "https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app",
-    title : "Learn",
-    description : "Learn about Next.js in an interactive course with&nbsp;quizzes!"
-  },
-  {
-    href : "https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app",
-    title : "Templates",
-    description : "Discover and deploy boilerplate example Next.js&nbsp;projects."
-  },
-  {
-    href : "https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app",
-    title : "Deploy",
-    description : "Instantly deploy your Next.js site to a shareable URLwith&nbsp;Vercel."
-  }
-]
+type Item = {
+  href : string;
+  title : string;
+  description : string;
+}
 
-export function Footer() {
+type Props = {
+  items: Item[];
+}
+
+export function Footer({items}:Props) {
   return (
         <footer className={classes.grid}>
-          {ITEMS.map((item)=>(
+          {items.map((item)=>(
             <Card key={item.href} href={item.href} title={item.title} description={item.description}></Card>
           ))}
         </footer>
