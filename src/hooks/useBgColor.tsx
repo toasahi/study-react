f75b0ed05d29e4cfbd5706d21555519290f5d749
toasahi <asa1313to@gmail.com>
2023-10-28 19:46:14 +0900

    ➕ useMemoの依存関係を配列に追加

--- src/hooks/useBgColor.tsx
+++ src/hooks/useBgColor.tsx
@@ -6,7 +6,7 @@ export const useBgColor = () =>{
 
   const bgColor = useMemo(()=>{
     return router.pathname === "/" ? "lightblue" : "beige"
-  },[])
+  },[router.pathname])
 
     useEffect(()=> {
       document.body.style.backgroundColor= bgColor
