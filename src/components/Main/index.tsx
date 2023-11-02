fb35b9c9287b7e18ad871bc1ee14cd2c481dea95
toasahi <asa1313to@gmail.com>
2023-11-02 21:00:07 +0900

    ♻️ 不要なインポートを削除

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -3,7 +3,6 @@ import { NextJsLogo } from '@/src/components/Logo/NextJsLogo'
 import { Description } from '@/src/components/Description'
 import classes from '@/src/components/Main/Main.module.css'
 import { Footer } from '@/src/components/Footer'
-import { Header } from '@/src/components/Header'
 import { useState, useCallback } from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
