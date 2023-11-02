f083d815f3dda1b7ac341b3fde85bd19e7f83620
toasahi <asa1313to@gmail.com>
2023-11-02 23:01:53 +0900

    🔥 不必要なコードを削除

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,7 @@ import '@/src/styles/globals.css'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
 
-import { useInputArray } from '@/src/hooks/useInputArray';
-import { useCounter } from '@/src/hooks/useCounter';
-
 const App = ({ Component, pageProps }: AppProps) =>{
-  const counter = useCounter()
-  const inputArray = useInputArray()
   return (
   <>
     <Head>
@@ -16,7 +11,7 @@ const App = ({ Component, pageProps }: AppProps) =>{
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
     </Head>
-    <Component {...pageProps} {...counter} {...inputArray} />
+    <Component {...pageProps} />
   </>)
 }
 
