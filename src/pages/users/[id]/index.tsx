946e2d3d7508a8f80ad12f37b5b0bf321d8b9dd2
toasahi <asa1313to@gmail.com>
2023-11-16 00:04:31 +0900

    ✨ ユーザー詳細ページでSSRを再現

--- src/pages/users/[id]/index.tsx
+++ src/pages/users/[id]/index.tsx
@@ -1,10 +1,38 @@
-import { User } from '@/src/components/User';
+import { GetServerSideProps } from 'next'
 
-const UserId = () => {
+import { User } from '@/src/components/User';
+import type { User as UserType} from '@/src/types/user';
+import { SWRConfig } from 'swr';
+
+export const getServerSideProps = (async (context) => {
+    const { id } = context.query
+    const API_URL=`https://jsonplaceholder.typicode.com/users/${id}`
+    const user = await fetch(API_URL)
+    const userData:UserType = await user.json()
+
+    return {
+        props: {
+            fallback: {
+                [API_URL]: userData,
+            },
+        },
+    };
+}) satisfies GetServerSideProps
+
+type Props = {
+    fallback : {
+        [key:string]: UserType
+    }
+}
+
+const UserId = (props: Props) => {
+    const { fallback } = props;
     return (
+        <SWRConfig value={{fallback}}>
         <div className="w-full">
             <User />
         </div>
+        </SWRConfig>
     );
 };
 
