ece8933ceeba5eb6800af2d47882fa8c5986fbf1
toasahi <asa1313to@gmail.com>
2023-12-02 08:55:24 +0900

    :sparkles: Add json-server
    json-serverへpathを変更

--- src/hooks/useFetchArray.tsx
+++ src/hooks/useFetchArray.tsx
@@ -3,12 +3,11 @@ import useSWRImmutable from 'swr/immutable';
 import type { Comment } from '@/src/types/comment';
 import type { Post } from '@/src/types/post';
 import type { User } from '@/src/types/user';
-
-const API_URL = 'https://jsonplaceholder.typicode.com';
+import { API_URL } from '@/src/utils/const';
 
 export const useComments = () => {
     const { data, error } = useSWRImmutable<Comment[], Error>(
-        `${API_URL}/comments`
+        `http://localhost:50100/comments`
     );
     return {
         data,
